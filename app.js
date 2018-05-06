dedfa9b25493b0f7c1b24fa56886338be67cef76
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-06 19:10:59 +0000

    added morgan -- logger

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
-
+const morgan = require("morgan");
 const app = express();
 
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
+app.use(morgan("dev"));
+
+//Routes which should handle requests
 app.use("/products", productRoutes);
 app.use("/orders", ordersRoutes);
 
