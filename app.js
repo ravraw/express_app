7ee2dc1d128390f20aedc6eea8ed141c454c7150
ravindra rawat <ravinrawat.wd@gmail.com>
2018-05-06 21:38:19 +0000

    cors error handled

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const morgan = require("morgan");
+//const bodyParser = require("body-parser");
 const app = express();
 
 const productRoutes = require("./api/routes/products");
 const ordersRoutes = require("./api/routes/orders");
 
 app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow_Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT,POST,PATCH,DELETE,GET");
+    return res.status(200).json({});
+  }
+  next();
+});
 
 //Routes which should handle requests
 app.use("/products", productRoutes);
